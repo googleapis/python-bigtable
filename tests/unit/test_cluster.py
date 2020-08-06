e74f88a14990eba4e82a9fef5fb64bae3454c55e
Raphael Long <rafilong@google.com>
2020-08-06 15:38:03 -0500

    refactor: create test_base_cluster (#84)
    Towards #78
    Stacked on #79

--- tests/unit/test_cluster.py
+++ tests/unit/test_cluster.py
@@ -19,48 +19,15 @@ import mock
 
 from ._testing import _make_credentials
 
-
-class MultiCallableStub(object):
-    """Stub for the grpc.UnaryUnaryMultiCallable interface."""
-
-    def __init__(self, method, channel_stub):
-        self.method = method
-        self.channel_stub = channel_stub
-
-    def __call__(self, request, timeout=None, metadata=None, credentials=None):
-        self.channel_stub.requests.append((self.method, request))
-
-        return self.channel_stub.responses.pop()
+from tests.unit.test_base_cluster import (
+    _Instance,
+    _Client,
+    ChannelStub,
+    TestClusterConstants,
+)
 
 
-class ChannelStub(object):
-    """Stub for the grpc.Channel interface."""
-
-    def __init__(self, responses=[]):
-        self.responses = responses
-        self.requests = []
-
-    def unary_unary(self, method, request_serializer=None, response_deserializer=None):
-        return MultiCallableStub(method, self)
-
-
-class TestCluster(unittest.TestCase):
-
-    PROJECT = "project"
-    INSTANCE_ID = "instance-id"
-    LOCATION_ID = "location-id"
-    CLUSTER_ID = "cluster-id"
-    LOCATION_ID = "location-id"
-    CLUSTER_NAME = (
-        "projects/" + PROJECT + "/instances/" + INSTANCE_ID + "/clusters/" + CLUSTER_ID
-    )
-    LOCATION_PATH = "projects/" + PROJECT + "/locations/"
-    SERVE_NODES = 5
-    OP_ID = 5678
-    OP_NAME = "operations/projects/{}/instances/{}/clusters/{}/operations/{}".format(
-        PROJECT, INSTANCE_ID, CLUSTER_ID, OP_ID
-    )
-
+class TestCluster(unittest.TestCase, TestClusterConstants):
     @staticmethod
     def _get_target_class():
         from google.cloud.bigtable.cluster import Cluster
@@ -90,136 +57,6 @@ class TestCluster(unittest.TestCase):
         self.assertIsNone(cluster.state)
         self.assertIsNone(cluster.serve_nodes)
         self.assertIsNone(cluster.default_storage_type)
-
-    def test_constructor_non_default(self):
-        from google.cloud.bigtable.enums import StorageType
-        from google.cloud.bigtable.enums import Cluster
-
-        STATE = Cluster.State.READY
-        STORAGE_TYPE_SSD = StorageType.SSD
-        client = _Client(self.PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-
-        cluster = self._make_one(
-            self.CLUSTER_ID,
-            instance,
-            location_id=self.LOCATION_ID,
-            _state=STATE,
-            serve_nodes=self.SERVE_NODES,
-            default_storage_type=STORAGE_TYPE_SSD,
-        )
-        self.assertEqual(cluster.cluster_id, self.CLUSTER_ID)
-        self.assertIs(cluster._instance, instance)
-        self.assertEqual(cluster.location_id, self.LOCATION_ID)
-        self.assertEqual(cluster.state, STATE)
-        self.assertEqual(cluster.serve_nodes, self.SERVE_NODES)
-        self.assertEqual(cluster.default_storage_type, STORAGE_TYPE_SSD)
-
-    def test_name_property(self):
-        credentials = _make_credentials()
-        client = self._make_client(
-            project=self.PROJECT, credentials=credentials, admin=True
-        )
-        instance = _Instance(self.INSTANCE_ID, client)
-        cluster = self._make_one(self.CLUSTER_ID, instance)
-
-        self.assertEqual(cluster.name, self.CLUSTER_NAME)
-
-    def test_from_pb_success(self):
-        from google.cloud.bigtable_admin_v2.proto import instance_pb2 as data_v2_pb2
-        from google.cloud.bigtable import enums
-
-        client = _Client(self.PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-
-        location = self.LOCATION_PATH + self.LOCATION_ID
-        state = enums.Cluster.State.RESIZING
-        storage_type = enums.StorageType.SSD
-        cluster_pb = data_v2_pb2.Cluster(
-            name=self.CLUSTER_NAME,
-            location=location,
-            state=state,
-            serve_nodes=self.SERVE_NODES,
-            default_storage_type=storage_type,
-        )
-
-        klass = self._get_target_class()
-        cluster = klass.from_pb(cluster_pb, instance)
-        self.assertIsInstance(cluster, klass)
-        self.assertEqual(cluster._instance, instance)
-        self.assertEqual(cluster.cluster_id, self.CLUSTER_ID)
-        self.assertEqual(cluster.location_id, self.LOCATION_ID)
-        self.assertEqual(cluster.state, state)
-        self.assertEqual(cluster.serve_nodes, self.SERVE_NODES)
-        self.assertEqual(cluster.default_storage_type, storage_type)
-
-    def test_from_pb_bad_cluster_name(self):
-        from google.cloud.bigtable_admin_v2.proto import instance_pb2 as data_v2_pb2
-
-        bad_cluster_name = "BAD_NAME"
-
-        cluster_pb = data_v2_pb2.Cluster(name=bad_cluster_name)
-
-        klass = self._get_target_class()
-        with self.assertRaises(ValueError):
-            klass.from_pb(cluster_pb, None)
-
-    def test_from_pb_instance_id_mistmatch(self):
-        from google.cloud.bigtable_admin_v2.proto import instance_pb2 as data_v2_pb2
-
-        ALT_INSTANCE_ID = "ALT_INSTANCE_ID"
-        client = _Client(self.PROJECT)
-        instance = _Instance(ALT_INSTANCE_ID, client)
-
-        self.assertNotEqual(self.INSTANCE_ID, ALT_INSTANCE_ID)
-        cluster_pb = data_v2_pb2.Cluster(name=self.CLUSTER_NAME)
-
-        klass = self._get_target_class()
-        with self.assertRaises(ValueError):
-            klass.from_pb(cluster_pb, instance)
-
-    def test_from_pb_project_mistmatch(self):
-        from google.cloud.bigtable_admin_v2.proto import instance_pb2 as data_v2_pb2
-
-        ALT_PROJECT = "ALT_PROJECT"
-        client = _Client(project=ALT_PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-
-        self.assertNotEqual(self.PROJECT, ALT_PROJECT)
-        cluster_pb = data_v2_pb2.Cluster(name=self.CLUSTER_NAME)
-
-        klass = self._get_target_class()
-        with self.assertRaises(ValueError):
-            klass.from_pb(cluster_pb, instance)
-
-    def test___eq__(self):
-        client = _Client(self.PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-        cluster1 = self._make_one(self.CLUSTER_ID, instance, self.LOCATION_ID)
-        cluster2 = self._make_one(self.CLUSTER_ID, instance, self.LOCATION_ID)
-        self.assertEqual(cluster1, cluster2)
-
-    def test___eq__type_differ(self):
-        client = _Client(self.PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-        cluster1 = self._make_one(self.CLUSTER_ID, instance, self.LOCATION_ID)
-        cluster2 = object()
-        self.assertNotEqual(cluster1, cluster2)
-
-    def test___ne__same_value(self):
-        client = _Client(self.PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-        cluster1 = self._make_one(self.CLUSTER_ID, instance, self.LOCATION_ID)
-        cluster2 = self._make_one(self.CLUSTER_ID, instance, self.LOCATION_ID)
-        comparison_val = cluster1 != cluster2
-        self.assertFalse(comparison_val)
-
-    def test___ne__(self):
-        client = _Client(self.PROJECT)
-        instance = _Instance(self.INSTANCE_ID, client)
-        cluster1 = self._make_one("cluster_id1", instance, self.LOCATION_ID)
-        cluster2 = self._make_one("cluster_id2", instance, self.LOCATION_ID)
-        self.assertNotEqual(cluster1, cluster2)
 
     def test_reload(self):
         from google.cloud.bigtable_admin_v2.gapic import bigtable_instance_admin_client
@@ -475,22 +312,3 @@ class TestCluster(unittest.TestCase):
         result = cluster.delete()
 
         self.assertEqual(result, expected_result)
-
-
-class _Instance(object):
-    def __init__(self, instance_id, client):
-        self.instance_id = instance_id
-        self._client = client
-
-    def __eq__(self, other):
-        return other.instance_id == self.instance_id and other._client == self._client
-
-
-class _Client(object):
-    def __init__(self, project):
-        self.project = project
-        self.project_name = "projects/" + self.project
-        self._operations_stub = mock.sentinel.operations_stub
-
-    def __eq__(self, other):
-        return other.project == self.project and other.project_name == self.project_name
