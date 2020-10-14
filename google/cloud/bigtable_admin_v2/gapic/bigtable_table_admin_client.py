18c5d8c6a0c22c8ebea41786dfaa6af9a42846c2
yoshi-automation <yoshi-automation@google.com>
2020-10-14 05:36:10 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google/cloud/bigtable_admin_v2/gapic/bigtable_table_admin_client.py
+++ google/cloud/bigtable_admin_v2/gapic/bigtable_table_admin_client.py
@@ -54,14 +54,9 @@ from google.protobuf import empty_pb2
 from google.protobuf import field_mask_pb2
 
 
-import pkg_resources
-
-try:
-    _GAPIC_LIBRARY_VERSION = pkg_resources.get_distribution(
-        "google-cloud-bigtable"
-    ).version
-except pkg_resources.DistributionNotFound:
-    _GAPIC_LIBRARY_VERSION = None
+_GAPIC_LIBRARY_VERSION = pkg_resources.get_distribution(
+    "google-cloud-bigtable",
+).version
 
 
 class BigtableTableAdminClient(object):
