731efdd43f6ea66f29a72d478a193201db509436
Chris Rossi <chris@archimedeanco.com>
2021-03-22 13:08:07 -0400

    Differentiate instance and cluster names for cmek test, so tests aren't stepping on each other's toes. Remove bogus backup with cmek test.

--- tests/system.py
+++ tests/system.py
@@ -466,7 +466,7 @@ class TestInstanceAdminAPI(unittest.TestCase):
         from google.cloud.bigtable.table import ClusterState
 
         _PRODUCTION = enums.Instance.Type.PRODUCTION
-        ALT_INSTANCE_ID = "dif" + UNIQUE_SUFFIX
+        ALT_INSTANCE_ID = "dif-cmek" + UNIQUE_SUFFIX
         instance = Config.CLIENT.instance(
             ALT_INSTANCE_ID, instance_type=_PRODUCTION, labels=LABELS
         )
@@ -1098,76 +1098,6 @@ class TestTableAdminAPI(unittest.TestCase):
         self.assertEqual(
             temp_table_backup.encryption_info.encryption_type,
             enums.EncryptionInfo.EncryptionType.GOOGLE_DEFAULT_ENCRYPTION,
-        )
-
-        # Testing `Backup.update_expire_time()` method
-        expire += 3600  # A one-hour change in the `expire_time` parameter
-        updated_time = datetime.datetime.utcfromtimestamp(expire)
-        temp_backup.update_expire_time(updated_time)
-        test = _datetime_to_pb_timestamp(updated_time)
-
-        # Testing `Backup.get()` method
-        temp_table_backup = temp_backup.get()
-        self.assertEqual(
-            test.seconds,
-            DatetimeWithNanoseconds.timestamp(temp_table_backup.expire_time),
-        )
-
-        # Testing `Table.restore()` and `Backup.retore()` methods
-        restored_table_id = "test-backup-table-restored"
-        restored_table = Config.INSTANCE_DATA.table(restored_table_id)
-        temp_table.restore(
-            restored_table_id, cluster_id=CLUSTER_ID_DATA, backup_id=temp_backup_id
-        ).result()
-        tables = Config.INSTANCE_DATA.list_tables()
-        self.assertIn(restored_table, tables)
-        restored_table.delete()
-
-    @pytest.mark.skipif(
-        not KMS_KEY_NAME, reason="requires KMS_KEY_NAME environment variable"
-    )
-    def test_backup_cmek(self):
-        from google.cloud._helpers import _datetime_to_pb_timestamp
-        from google.cloud.bigtable import enums
-
-        temp_table_id = "test-backup-table"
-        temp_table = Config.INSTANCE_DATA.table(temp_table_id)
-        temp_table.create()
-        self.tables_to_delete.append(temp_table)
-
-        temp_backup_id = "test-backup"
-
-        # TODO: consider using `datetime.datetime.now().timestamp()`
-        #  when support for Python 2 is fully dropped
-        expire = int(time.mktime(datetime.datetime.now().timetuple())) + 604800
-
-        # Testing `Table.backup()` factory
-        temp_backup = temp_table.backup(
-            temp_backup_id,
-            cluster_id=CLUSTER_ID_DATA,
-            expire_time=datetime.datetime.utcfromtimestamp(expire),
-        )
-
-        # Sanity check for `Backup.exists()` method
-        self.assertFalse(temp_backup.exists())
-
-        # Testing `Backup.create()` method
-        temp_backup.create().result()
-
-        # Implicit testing of `Backup.delete()` method
-        self.backups_to_delete.append(temp_backup)
-
-        # Testing `Backup.exists()` method
-        self.assertTrue(temp_backup.exists())
-
-        # Testing `Table.list_backups()` method
-        temp_table_backup = temp_table.list_backups()[0]
-        self.assertEqual(temp_backup_id, temp_table_backup.backup_id)
-        self.assertEqual(CLUSTER_ID_DATA, temp_table_backup.cluster)
-        self.assertEqual(expire, temp_table_backup.expire_time.seconds)
-        self.assertEqual(
-            temp_table_backup.encryption_info.encryption_type,
-            enums.EncryptionInfo.EncryptionType.CUSTOMER_MANAGED_ENCRYPTION,
         )
 
         # Testing `Backup.update_expire_time()` method
