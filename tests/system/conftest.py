095782278455ee5df5039b4c45a62a2c11092af4
Chris Wilcox <crwilcox@google.com>
2021-10-11 14:36:27 -0700

    test: address issue with fixtures not raising exceptions under emulator, flag tests as not emulator compatible

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -146,13 +146,13 @@ def data_instance_populated(
     serve_nodes,
     in_emulator,
 ):
+    instance = admin_client.instance(data_instance_id, labels=instance_labels)
+    cluster = instance.cluster(
+        data_cluster_id, location_id=location_id, serve_nodes=serve_nodes,
+    )
     # Emulator does not support instance admin operations (create / delete).
     # See: https://cloud.google.com/bigtable/docs/emulator
     if not in_emulator:
-        instance = admin_client.instance(data_instance_id, labels=instance_labels)
-        cluster = instance.cluster(
-            data_cluster_id, location_id=location_id, serve_nodes=serve_nodes,
-        )
         operation = instance.create(clusters=[cluster])
         operation.result(timeout=30)
 
