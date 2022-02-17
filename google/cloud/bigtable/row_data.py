93f61611fb9eae3de61a4476b27da2b9952b56b9
Mariatta Wijaya <mariattaw@google.com>
2022-02-17 13:05:20 -0800

    Added a todo comment

--- google/cloud/bigtable/row_data.py
+++ google/cloud/bigtable/row_data.py
@@ -624,6 +624,9 @@ class _ReadRowsRequestManager(object):
 
     def build_updated_request(self):
         """Updates the given message request as per last scanned key"""
+
+        # TODO: Generalize this to ensure fields don't get rewritten when retrying the request
+
         r_kwargs = {
             "table_name": self.message.table_name,
             "filter": self.message.filter,
