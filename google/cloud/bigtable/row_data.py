5253d920fd440480be71b123d4f97701360cac9b
Mariatta Wijaya <mariattaw@google.com>
2022-04-04 11:09:17 -0700

    fix: Prevent sending full table scan when retrying
    Update the retry logic. Don't send empty row_key and empty row_ranges
    if the original message didn't ask for those.
    
    Closes internal issue 214449800

--- google/cloud/bigtable/row_data.py
+++ google/cloud/bigtable/row_data.py
@@ -643,6 +643,16 @@ class _ReadRowsRequestManager(object):
         else:
             row_keys = self._filter_rows_keys()
             row_ranges = self._filter_row_ranges()
+
+            if len(row_keys) == 0 and len(row_ranges) == 0:
+                # Avoid sending empty row_keys and row_ranges
+                # if that was not the intention
+                if (
+                    len(self.message.rows.row_ranges) > 0
+                    or len(self.message.rows.row_keys) > 0
+                ):
+                    raise StopIteration
+
             resume_request.rows = data_v2_pb2.RowSet(
                 row_keys=row_keys, row_ranges=row_ranges
             )
