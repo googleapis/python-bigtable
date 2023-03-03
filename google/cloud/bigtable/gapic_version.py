11b34935e99e00150dff423c71aafc9933310964
Daniel Sanche <d.sanche14@gmail.com>
2023-03-03 10:44:57 -0800

    fixed missing files

--- google/cloud/bigtable/gapic_version.py
+++ google/cloud/bigtable/gapic_version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2023 Google LLC
+# Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,11 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from google.cloud.bigtable import gapic_version as package_version
-
-__version__ = package_version.__version__
-
-from google.cloud.bigtable.client import BigtableDataClient
-from google.cloud.bigtable.client import Table
-
-__all__ = ("BigtableDataClient", "Table")
+__version__ = "2.15.0"  # {x-release-please-version}
