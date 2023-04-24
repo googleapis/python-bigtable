2de0c5e32fe35bbc69cd34841c48a29b23b1a190
Daniel Sanche <d.sanche14@gmail.com>
2023-04-24 12:48:41 -0700

    reverted submodule update

--- .kokoro/trampoline.sh
+++ .kokoro/trampoline.sh
@@ -24,7 +24,5 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-git submodule update --init --recursive
-
 $(dirname $0)/populate-secrets.sh # Secret Manager secrets.
 python3 "${KOKORO_GFILE_DIR}/trampoline_v1.py"
