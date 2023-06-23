ec2b9835d3fd050414755b30a769a769d74f802a
Daniel Sanche <sanche@google.com>
2023-06-23 11:29:15 -0700

    feat: sharded read rows (#766)

--- google/cloud/bigtable/client.py
+++ google/cloud/bigtable/client.py
@@ -38,6 +38,7 @@ from google.cloud.bigtable_v2.services.bigtable.async_client import DEFAULT_CLIE
 from google.cloud.bigtable_v2.services.bigtable.transports.pooled_grpc_asyncio import (
     PooledBigtableGrpcAsyncIOTransport,
 )
+from google.cloud.bigtable_v2.types.bigtable import PingAndWarmRequest
 from google.cloud.client import ClientWithProject
 from google.api_core.exceptions import GoogleAPICallError
 from google.api_core import retry_async as retries
@@ -50,10 +51,14 @@ from google.api_core import client_options as client_options_lib
 from google.cloud.bigtable.row import Row
 from google.cloud.bigtable.read_rows_query import ReadRowsQuery
 from google.cloud.bigtable.iterators import ReadRowsIterator
+from google.cloud.bigtable.exceptions import FailedQueryShardError
+from google.cloud.bigtable.exceptions import ShardedReadRowsExceptionGroup
+
 from google.cloud.bigtable.mutations import Mutation, RowMutationEntry
 from google.cloud.bigtable._mutate_rows import _MutateRowsOperation
 from google.cloud.bigtable._helpers import _make_metadata
 from google.cloud.bigtable._helpers import _convert_retry_deadline
+from google.cloud.bigtable._helpers import _attempt_timeout_generator
 
 from google.cloud.bigtable.read_modify_write_rules import ReadModifyWriteRule
 from google.cloud.bigtable.row_filters import RowFilter
@@ -64,6 +69,10 @@ from google.cloud.bigtable.row_filters import RowFilterChain
 if TYPE_CHECKING:
     from google.cloud.bigtable.mutations_batcher import MutationsBatcher
     from google.cloud.bigtable import RowKeySamples
+    from google.cloud.bigtable import ShardedQuery
+
+# used by read_rows_sharded to limit how many requests are attempted in parallel
+CONCURRENCY_LIMIT = 10
 
 
 class BigtableDataClient(ClientWithProject):
@@ -190,10 +199,13 @@ class BigtableDataClient(ClientWithProject):
             - sequence of results or exceptions from the ping requests
         """
         ping_rpc = channel.unary_unary(
-            "/google.bigtable.v2.Bigtable/PingAndWarmChannel"
+            "/google.bigtable.v2.Bigtable/PingAndWarm",
+            request_serializer=PingAndWarmRequest.serialize,
         )
         tasks = [ping_rpc({"name": n}) for n in self._active_instances]
-        return await asyncio.gather(*tasks, return_exceptions=True)
+        result = await asyncio.gather(*tasks, return_exceptions=True)
+        # return None in place of empty successful responses
+        return [r or None for r in result]
 
     async def _manage_channel(
         self,
@@ -532,22 +544,79 @@ class Table:
 
     async def read_rows_sharded(
         self,
-        query_list: list[ReadRowsQuery] | list[dict[str, Any]],
+        sharded_query: ShardedQuery,
         *,
-        limit: int | None,
-        operation_timeout: int | float | None = 60,
+        operation_timeout: int | float | None = None,
         per_request_timeout: int | float | None = None,
-    ) -> ReadRowsIterator:
+    ) -> list[Row]:
         """
-        Runs a sharded query in parallel
+        Runs a sharded query in parallel, then return the results in a single list.
+        Results will be returned in the order of the input queries.
 
-        Each query in query list will be run concurrently, with results yielded as they are ready
-        yielded results may be out of order
+        This function is intended to be run on the results on a query.shard() call:
+
+        ```
+        table_shard_keys = await table.sample_row_keys()
+        query = ReadRowsQuery(...)
+        shard_queries = query.shard(table_shard_keys)
+        results = await table.read_rows_sharded(shard_queries)
+        ```
 
         Args:
-            - query_list: a list of queries to run in parallel
+            - sharded_query: a sharded query to execute
+        Raises:
+            - ShardedReadRowsExceptionGroup: if any of the queries failed
+            - ValueError: if the query_list is empty
         """
-        raise NotImplementedError
+        if not sharded_query:
+            raise ValueError("empty sharded_query")
+        # reduce operation_timeout between batches
+        operation_timeout = operation_timeout or self.default_operation_timeout
+        per_request_timeout = (
+            per_request_timeout or self.default_per_request_timeout or operation_timeout
+        )
+        timeout_generator = _attempt_timeout_generator(
+            operation_timeout, operation_timeout
+        )
+        # submit shards in batches if the number of shards goes over CONCURRENCY_LIMIT
+        batched_queries = [
+            sharded_query[i : i + CONCURRENCY_LIMIT]
+            for i in range(0, len(sharded_query), CONCURRENCY_LIMIT)
+        ]
+        # run batches and collect results
+        results_list = []
+        error_dict = {}
+        shard_idx = 0
+        for batch in batched_queries:
+            batch_operation_timeout = next(timeout_generator)
+            routine_list = [
+                self.read_rows(
+                    query,
+                    operation_timeout=batch_operation_timeout,
+                    per_request_timeout=min(
+                        per_request_timeout, batch_operation_timeout
+                    ),
+                )
+                for query in batch
+            ]
+            batch_result = await asyncio.gather(*routine_list, return_exceptions=True)
+            for result in batch_result:
+                if isinstance(result, Exception):
+                    error_dict[shard_idx] = result
+                else:
+                    results_list.extend(result)
+                shard_idx += 1
+        if error_dict:
+            # if any sub-request failed, raise an exception instead of returning results
+            raise ShardedReadRowsExceptionGroup(
+                [
+                    FailedQueryShardError(idx, sharded_query[idx], e)
+                    for idx, e in error_dict.items()
+                ],
+                results_list,
+                len(sharded_query),
+            )
+        return results_list
 
     async def row_exists(
         self,
@@ -577,12 +646,11 @@ class Table:
         )
         return len(results) > 0
 
-    async def sample_keys(
+    async def sample_row_keys(
         self,
         *,
-        operation_timeout: int | float | None = 60,
-        per_sample_timeout: int | float | None = 10,
-        per_request_timeout: int | float | None = None,
+        operation_timeout: float | None = None,
+        per_request_timeout: float | None = None,
     ) -> RowKeySamples:
         """
         Return a set of RowKeySamples that delimit contiguous sections of the table of
@@ -590,7 +658,7 @@ class Table:
 
         RowKeySamples output can be used with ReadRowsQuery.shard() to create a sharded query that
         can be parallelized across multiple backend nodes read_rows and read_rows_stream
-        requests will call sample_keys internally for this purpose when sharding is enabled
+        requests will call sample_row_keys internally for this purpose when sharding is enabled
 
         RowKeySamples is simply a type alias for list[tuple[bytes, int]]; a list of
             row_keys, along with offset positions in the table
@@ -598,11 +666,61 @@ class Table:
         Returns:
             - a set of RowKeySamples the delimit contiguous sections of the table
         Raises:
-            - DeadlineExceeded: raised after operation timeout
-                will be chained with a RetryExceptionGroup containing all GoogleAPIError
-                exceptions from any retries that failed
+            - GoogleAPICallError: if the sample_row_keys request fails
         """
-        raise NotImplementedError
+        # prepare timeouts
+        operation_timeout = operation_timeout or self.default_operation_timeout
+        per_request_timeout = per_request_timeout or self.default_per_request_timeout
+
+        if operation_timeout <= 0:
+            raise ValueError("operation_timeout must be greater than 0")
+        if per_request_timeout is not None and per_request_timeout <= 0:
+            raise ValueError("per_request_timeout must be greater than 0")
+        if per_request_timeout is not None and per_request_timeout > operation_timeout:
+            raise ValueError(
+                "per_request_timeout must not be greater than operation_timeout"
+            )
+        attempt_timeout_gen = _attempt_timeout_generator(
+            per_request_timeout, operation_timeout
+        )
+        # prepare retryable
+        predicate = retries.if_exception_type(
+            core_exceptions.DeadlineExceeded,
+            core_exceptions.ServiceUnavailable,
+        )
+        transient_errors = []
+
+        def on_error_fn(exc):
+            # add errors to list if retryable
+            if predicate(exc):
+                transient_errors.append(exc)
+
+        retry = retries.AsyncRetry(
+            predicate=predicate,
+            timeout=operation_timeout,
+            initial=0.01,
+            multiplier=2,
+            maximum=60,
+            on_error=on_error_fn,
+            is_stream=False,
+        )
+
+        # prepare request
+        metadata = _make_metadata(self.table_name, self.app_profile_id)
+
+        async def execute_rpc():
+            results = await self.client._gapic_client.sample_row_keys(
+                table_name=self.table_name,
+                app_profile_id=self.app_profile_id,
+                timeout=next(attempt_timeout_gen),
+                metadata=metadata,
+            )
+            return [(s.row_key, s.offset_bytes) async for s in results]
+
+        wrapped_fn = _convert_retry_deadline(
+            retry(execute_rpc), operation_timeout, transient_errors
+        )
+        return await wrapped_fn()
 
     def mutations_batcher(self, **kwargs) -> MutationsBatcher:
         """
@@ -896,16 +1014,17 @@ class Table:
         """
         Called to close the Table instance and release any resources held by it.
         """
+        self._register_instance_task.cancel()
         await self.client._remove_instance_registration(self.instance_id, self)
 
     async def __aenter__(self):
         """
         Implement async context manager protocol
 
-        Register this instance with the client, so that
+        Ensure registration task has time to run, so that
         grpc channels will be warmed for the specified instance
         """
-        await self.client._register_instance(self.instance_id, self)
+        await self._register_instance_task
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
--- google/cloud/bigtable/read_rows_query.py
+++ google/cloud/bigtable/read_rows_query.py
@@ -14,11 +14,15 @@
 #
 from __future__ import annotations
 from typing import TYPE_CHECKING, Any
+from bisect import bisect_left
+from bisect import bisect_right
+from collections import defaultdict
 from dataclasses import dataclass
 from google.cloud.bigtable.row_filters import RowFilter
 
 if TYPE_CHECKING:
     from google.cloud.bigtable import RowKeySamples
+    from google.cloud.bigtable import ShardedQuery
 
 
 @dataclass
@@ -27,6 +31,9 @@ class _RangePoint:
 
     key: bytes
     is_inclusive: bool
+
+    def __hash__(self) -> int:
+        return hash((self.key, self.is_inclusive))
 
 
 @dataclass
@@ -80,6 +87,44 @@ class RowRange:
             output[key] = self.end.key
         return output
 
+    def __hash__(self) -> int:
+        return hash((self.start, self.end))
+
+    @classmethod
+    def _from_dict(cls, data: dict[str, bytes]) -> RowRange:
+        """Creates a RowRange from a dictionary"""
+        start_key = data.get("start_key_closed", data.get("start_key_open"))
+        end_key = data.get("end_key_closed", data.get("end_key_open"))
+        start_is_inclusive = "start_key_closed" in data if start_key else None
+        end_is_inclusive = "end_key_closed" in data if end_key else None
+        return cls(
+            start_key,
+            end_key,
+            start_is_inclusive,
+            end_is_inclusive,
+        )
+
+    @classmethod
+    def _from_points(
+        cls, start: _RangePoint | None, end: _RangePoint | None
+    ) -> RowRange:
+        """Creates a RowRange from two RangePoints"""
+        kwargs: dict[str, Any] = {}
+        if start is not None:
+            kwargs["start_key"] = start.key
+            kwargs["start_is_inclusive"] = start.is_inclusive
+        if end is not None:
+            kwargs["end_key"] = end.key
+            kwargs["end_is_inclusive"] = end.is_inclusive
+        return cls(**kwargs)
+
+    def __bool__(self) -> bool:
+        """
+        Empty RowRanges (representing a full table scan) are falsy, because
+        they can be substituted with None. Non-empty RowRanges are truthy.
+        """
+        return self.start is not None or self.end is not None
+
 
 class ReadRowsQuery:
     """
@@ -91,7 +136,7 @@ class ReadRowsQuery:
         row_keys: list[str | bytes] | str | bytes | None = None,
         row_ranges: list[RowRange] | RowRange | None = None,
         limit: int | None = None,
-        row_filter: RowFilter | None = None,
+        row_filter: RowFilter | dict[str, Any] | None = None,
     ):
         """
         Create a new ReadRowsQuery
@@ -105,7 +150,7 @@ class ReadRowsQuery:
           - row_filter: a RowFilter to apply to the query
         """
         self.row_keys: set[bytes] = set()
-        self.row_ranges: list[RowRange | dict[str, bytes]] = []
+        self.row_ranges: set[RowRange] = set()
         if row_ranges is not None:
             if isinstance(row_ranges, RowRange):
                 row_ranges = [row_ranges]
@@ -197,18 +242,133 @@ class ReadRowsQuery:
         """
         if not (isinstance(row_range, dict) or isinstance(row_range, RowRange)):
             raise ValueError("row_range must be a RowRange or dict")
-        self.row_ranges.append(row_range)
+        if isinstance(row_range, dict):
+            row_range = RowRange._from_dict(row_range)
+        self.row_ranges.add(row_range)
 
-    def shard(self, shard_keys: "RowKeySamples" | None = None) -> list[ReadRowsQuery]:
+    def shard(self, shard_keys: RowKeySamples) -> ShardedQuery:
         """
         Split this query into multiple queries that can be evenly distributed
-        across nodes and be run in parallel
+        across nodes and run in parallel
 
         Returns:
-            - a list of queries that represent a sharded version of the original
-              query (if possible)
+            - a ShardedQuery that can be used in sharded_read_rows calls
+        Raises:
+            - AttributeError if the query contains a limit
         """
-        raise NotImplementedError
+        if self.limit is not None:
+            raise AttributeError("Cannot shard query with a limit")
+        if len(self.row_keys) == 0 and len(self.row_ranges) == 0:
+            # empty query represents full scan
+            # ensure that we have at least one key or range
+            full_scan_query = ReadRowsQuery(
+                row_ranges=RowRange(), row_filter=self.filter
+            )
+            return full_scan_query.shard(shard_keys)
+
+        sharded_queries: dict[int, ReadRowsQuery] = defaultdict(
+            lambda: ReadRowsQuery(row_filter=self.filter)
+        )
+        # the split_points divde our key space into segments
+        # each split_point defines last key that belongs to a segment
+        # our goal is to break up the query into subqueries that each operate in a single segment
+        split_points = [sample[0] for sample in shard_keys if sample[0]]
+
+        # handle row_keys
+        # use binary search to find the segment that each key belongs to
+        for this_key in list(self.row_keys):
+            # bisect_left: in case of exact match, pick left side (keys are inclusive ends)
+            segment_index = bisect_left(split_points, this_key)
+            sharded_queries[segment_index].add_key(this_key)
+
+        # handle row_ranges
+        for this_range in self.row_ranges:
+            # defer to _shard_range helper
+            for segment_index, added_range in self._shard_range(
+                this_range, split_points
+            ):
+                sharded_queries[segment_index].add_range(added_range)
+        # return list of queries ordered by segment index
+        # pull populated segments out of sharded_queries dict
+        keys = sorted(list(sharded_queries.keys()))
+        # return list of queries
+        return [sharded_queries[k] for k in keys]
+
+    @staticmethod
+    def _shard_range(
+        orig_range: RowRange, split_points: list[bytes]
+    ) -> list[tuple[int, RowRange]]:
+        """
+        Helper function for sharding row_range into subranges that fit into
+        segments of the key-space, determined by split_points
+
+        Args:
+          - orig_range: a row range to split
+          - split_points: a list of row keys that define the boundaries of segments.
+                each point represents the inclusive end of a segment
+        Returns:
+          - a list of tuples, containing a segment index and a new sub-range.
+        """
+        # 1. find the index of the segment the start key belongs to
+        if orig_range.start is None:
+            # if range is open on the left, include first segment
+            start_segment = 0
+        else:
+            # use binary search to find the segment the start key belongs to
+            # bisect method determines how we break ties when the start key matches a split point
+            # if inclusive, bisect_left to the left segment, otherwise bisect_right
+            bisect = bisect_left if orig_range.start.is_inclusive else bisect_right
+            start_segment = bisect(split_points, orig_range.start.key)
+
+        # 2. find the index of the segment the end key belongs to
+        if orig_range.end is None:
+            # if range is open on the right, include final segment
+            end_segment = len(split_points)
+        else:
+            # use binary search to find the segment the end key belongs to.
+            end_segment = bisect_left(
+                split_points, orig_range.end.key, lo=start_segment
+            )
+            # note: end_segment will always bisect_left, because split points represent inclusive ends
+            # whether the end_key is includes the split point or not, the result is the same segment
+        # 3. create new range definitions for each segment this_range spans
+        if start_segment == end_segment:
+            # this_range is contained in a single segment.
+            # Add this_range to that segment's query only
+            return [(start_segment, orig_range)]
+        else:
+            results: list[tuple[int, RowRange]] = []
+            # this_range spans multiple segments. Create a new range for each segment's query
+            # 3a. add new range for first segment this_range spans
+            # first range spans from start_key to the split_point representing the last key in the segment
+            last_key_in_first_segment = split_points[start_segment]
+            start_range = RowRange._from_points(
+                start=orig_range.start,
+                end=_RangePoint(last_key_in_first_segment, is_inclusive=True),
+            )
+            results.append((start_segment, start_range))
+            # 3b. add new range for last segment this_range spans
+            # we start the final range using the end key from of the previous segment, with is_inclusive=False
+            previous_segment = end_segment - 1
+            last_key_before_segment = split_points[previous_segment]
+            end_range = RowRange._from_points(
+                start=_RangePoint(last_key_before_segment, is_inclusive=False),
+                end=orig_range.end,
+            )
+            results.append((end_segment, end_range))
+            # 3c. add new spanning range to all segments other than the first and last
+            for this_segment in range(start_segment + 1, end_segment):
+                prev_segment = this_segment - 1
+                prev_end_key = split_points[prev_segment]
+                this_end_key = split_points[prev_segment + 1]
+                new_range = RowRange(
+                    start_key=prev_end_key,
+                    start_is_inclusive=False,
+                    end_key=this_end_key,
+                    end_is_inclusive=True,
+                )
+                results.append((this_segment, new_range))
+            return results
 
     def _to_dict(self) -> dict[str, Any]:
         """
@@ -221,11 +381,7 @@ class ReadRowsQuery:
             row_ranges.append(dict_range)
         row_keys = list(self.row_keys)
         row_keys.sort()
-        row_set: dict[str, Any] = {}
-        if row_keys:
-            row_set["row_keys"] = row_keys
-        if row_ranges:
-            row_set["row_ranges"] = row_ranges
+        row_set = {"row_keys": row_keys, "row_ranges": row_ranges}
         final_dict: dict[str, Any] = {
             "rows": row_set,
         }
@@ -237,3 +393,31 @@ class ReadRowsQuery:
         if self.limit is not None:
             final_dict["rows_limit"] = self.limit
         return final_dict
+
+    def __eq__(self, other):
+        """
+        RowRanges are equal if they have the same row keys, row ranges,
+        filter and limit, or if they both represent a full scan with the
+        same filter and limit
+        """
+        if not isinstance(other, ReadRowsQuery):
+            return False
+        # empty queries are equal
+        if len(self.row_keys) == 0 and len(other.row_keys) == 0:
+            this_range_empty = len(self.row_ranges) == 0 or all(
+                [bool(r) is False for r in self.row_ranges]
+            )
+            other_range_empty = len(other.row_ranges) == 0 or all(
+                [bool(r) is False for r in other.row_ranges]
+            )
+            if this_range_empty and other_range_empty:
+                return self.filter == other.filter and self.limit == other.limit
+        return (
+            self.row_keys == other.row_keys
+            and self.row_ranges == other.row_ranges
+            and self.filter == other.filter
+            and self.limit == other.limit
+        )
+
+    def __repr__(self):
+        return f"ReadRowsQuery(row_keys={list(self.row_keys)}, row_ranges={list(self.row_ranges)}, row_filter={self.filter}, limit={self.limit})"
