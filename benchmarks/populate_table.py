28cc1f4c219ffc0b8e84f469b1078f137adf885c
Daniel Sanche <d.sanche14@gmail.com>
2023-07-24 16:41:32 -0700

    finished table population

--- benchmarks/populate_table.py
+++ benchmarks/populate_table.py
@@ -1,11 +1,10 @@
 import random
 import asyncio
 import string
-import time
+from tqdm import tqdm
 
 from google.cloud.bigtable.data import SetCell
 from google.cloud.bigtable.data import RowMutationEntry
-from google.cloud.bigtable.data import TableAsync
 
 # the size of each value
 FIELD_SIZE = 100
@@ -24,9 +23,11 @@ BULK_SIZE = 1000
 # How many shards to use for the table population.
 POPULATE_SHARD_COUNT = 10
 
+
 def random_value():
     valid_chars = list(string.ascii_letters + "-/_")
     return "".join(random.choices(valid_chars, k=FIELD_SIZE))
+
 
 async def populate_table(table, table_size=100_000):
     """
@@ -38,29 +39,31 @@ async def populate_table(table, table_size=100_000):
         each shard has the same number of entries.
     """
     shard_size = max(table_size // POPULATE_SHARD_COUNT, 1)
-    print(f"Populating table {table.table_id} with {shard_size*POPULATE_SHARD_COUNT} entries...")
-    for shard_idx in range(POPULATE_SHARD_COUNT):
-        print(f"start: {shard_idx * shard_size}, end: {(shard_idx + 1) * shard_size}")
-        await _populate_shard(table, shard_idx * shard_size, (shard_idx + 1) * shard_size)
+    print(f"Populating table {table.table_id}...")
+    async with table.mutations_batcher() as batcher:
+        with tqdm(total=table_size) as pbar:
+            task_list = []
+            for shard_idx in range(POPULATE_SHARD_COUNT):
+                new_task = asyncio.create_task(_populate_shard(batcher, shard_idx * shard_size, (shard_idx + 1) * shard_size, pbar))
+                task_list.append(new_task)
+            await asyncio.gather(*task_list)
 
-async def _populate_shard(table, begin:int, end:int):
-    entry_list = []
+
+async def _populate_shard(batcher, begin:int, end:int, pbar=None):
     for idx in range(begin, end):
         row_key = f"user{str(idx).zfill(KEY_WIDTH)}"
         row_mutations = [SetCell(COLUMN_FAMILY, f"field{qual}", random_value()) for qual in range(NUM_QUALIFIERS)]
         entry = RowMutationEntry(row_key, row_mutations)
-        entry_list.append(entry)
-    await table.bulk_mutate_rows(entry_list)
+        await batcher.append(entry)
+        if pbar:
+            pbar.update(1)
+
 
 async def async_main():
     from google.cloud.bigtable.data import BigtableDataClientAsync
     client = BigtableDataClientAsync()
     table = client.get_table("sanche-test", "benchmarks")
-    # await populate_shard(table, 0, 1000)
     await populate_table(table)
 
 if __name__ == "__main__":
     asyncio.run(async_main())
-
-
-
