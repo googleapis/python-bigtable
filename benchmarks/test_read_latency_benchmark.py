a19123612fd4dc72009fc26b20ddd818fb2ec358
Daniel Sanche <d.sanche14@gmail.com>
2023-07-25 13:03:16 -0700

    added some basic throughput code

--- benchmarks/test_read_latency_benchmark.py
+++ benchmarks/test_read_latency_benchmark.py
@@ -16,8 +16,11 @@ import pytest
 import pytest_asyncio
 import uuid
 import os
+import random
+import time
 
 from populate_table import populate_table
+from populate_table import KEY_WIDTH
 
 
 @pytest.fixture(scope="session")
@@ -43,15 +46,57 @@ def init_table_id():
 async def populated_table(table):
     """
     populate a table with benchmark data
+
+    Table details:
+      - 10,000 rows
+      - single column family
+      - 10 column qualifiers
     """
     user_specified_table = os.getenv("BIGTABLE_TEST_TABLE")
     if table.table_id != user_specified_table:
         # only populate table if it is auto-generated
         # user-specified tables are assumed to be pre-populated
-        await populate_table(table)
+        await populate_table(table, 10_000)
     yield table
 
 
 @pytest.mark.asyncio
-async def test_read_latency_benchmark(populated_table):
-    print(populated_table.table_id)
+async def test_scan_throughput_benchmark(populated_table, duration=5):
+    """
+    This benchmark measures the throughput of read_rows against
+    a typical table
+
+    The benchmark will:
+      - for each `scan_size` in `[100, 1000, 10_000]`, execute the following loop
+        for `duration` seconds:
+        - pick one of the 10,000 keys at random, with uniform probability
+            - keys within `scan_size` of the end of the table are excluded from sampling
+        - scan `scan_size` rows starting at the chosen key
+
+    The benchmark will report throughput in rows per second for each `scan_size` value.
+
+    Table details:
+      - 10,000 rows
+      - single column family
+      - 10 column qualifiers
+    """
+    from google.cloud.bigtable.data import ReadRowsQuery
+    from google.cloud.bigtable.data import RowRange
+    from google.api_core.exceptions import DeadlineExceeded
+    for scan_size in [100, 1000, 10_000]:
+        print(f"running scan throughput benchmark with scan_size={scan_size}")
+        deadline = time.monotonic() + duration
+        total_rows = 0
+        while time.monotonic() < deadline:
+            start_idx = random.randint(0, max(10_000 - scan_size, 0))
+            start_key = f"user{str(start_idx).zfill(KEY_WIDTH)}"
+            query = ReadRowsQuery(row_ranges=RowRange(start_key=start_key), limit=scan_size)
+            try:
+                results = await populated_table.read_rows(query, operation_timeout=deadline - time.monotonic())
+                total_rows += len(results)
+            except DeadlineExceeded as e:
+                exc_group = e.__cause__
+                if exc_group and any(not isinstance(exc, DeadlineExceeded) for exc in exc_group.exceptions):
+                    # found error other than deadline exceeded
+                    raise
+        print(f"total rows: {total_rows}")
