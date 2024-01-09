da33429f038ca0b72262ade53de3e6bb5ceb1370
Daniel Sanche <d.sanche14@gmail.com>
2024-01-09 15:06:26 -0800

    added view

--- google/cloud/bigtable/data/_metrics/handlers/gcp_exporter.py
+++ google/cloud/bigtable/data/_metrics/handlers/gcp_exporter.py
@@ -13,18 +13,30 @@
 # limitations under the License.
 from opentelemetry import metrics
 from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics import view
 from opentelemetry.sdk.metrics.export import (
-    PeriodicExportingMetricReader,
+    PeriodicExportingMetricReader, MetricExporter
 )
-from opentelemetry.exporter.cloud_monitoring import (
-    CloudMonitoringMetricsExporter,
-)
+# from opentelemetry.exporter.cloud_monitoring import (
+#     CloudMonitoringMetricsExporter,
+# )
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
-from google.cloud.bigtable.data._metrics.handlers.opentelemetry import OpenTelemetryExporterHandler
+from google.cloud.bigtable.data._metrics.handlers.opentelemetry import OpenTelemetryMetricsHandler
 
 
-class GCPOpenTelemetryExporterHandler(OpenTelemetryExporterHandler):
+class TestExporter(MetricExporter):
+    def export(self, metric_records, timeout_millis=10_000, **kwargs):
+        print("exporting", metric_records)
+
+    def shutdown(self, **kwargs):
+        print("shutting down")
+
+    def force_flush(self, timeout_millis=None):
+        print("flushing")
+
+
+class GCPOpenTelemetryExporterHandler(OpenTelemetryMetricsHandler):
     def __init__(
         self,
         *args,
@@ -34,22 +46,35 @@ class GCPOpenTelemetryExporterHandler(OpenTelemetryExporterHandler):
         resource = Resource(attributes={
             SERVICE_NAME: "your-service-name",
         })
+        # create views
+        millis_aggregation = view.ExplicitBucketHistogramAggregation(
+            [0.0, 0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0,
+             13.0, 16.0, 20.0, 25.0, 30.0, 40.0, 50.0, 65.0, 80.0, 100.0, 130.0, 160.0, 200.0,
+             250.0, 300.0, 400.0, 500.0, 650.0, 800.0, 1000.0, 2000.0, 5000.0, 10000.0,
+             20000.0, 50000.0, 100000.0]
+        )
+        # retry_acount_aggregation = view.ExplicitBucketHistorgramAggregation(
+        #     [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 15.0, 20.0, 30.0, 40.0, 50.0, 100.0]
+        # )
+        operation_latencies_view = view.View(instrument_name="operation_latencies", aggregation=millis_aggregation, name="bigtable.googleapis.com/internal/client/operation_latencies")
         # writes metrics into GCP timeseries objects
-        exporter = CloudMonitoringMetricsExporter(prefix="bigtable.googleapis.com/internal/client/")
+        exporter = TestExporter()
+        # exporter = CloudMonitoringMetricsExporter(prefix="bigtable.googleapis.com/internal/client/")
         # periodically executes exporter
-        gcp_reader = PeriodicExportingMetricReader(exporter)
-        metrics.set_meter_provider(MeterProvider(resource=resource, metric_readers=[gcp_reader]))
+        gcp_reader = PeriodicExportingMetricReader(exporter, export_interval_millis=1000)
+        meter_provider = MeterProvider(resource=resource, metric_readers=[gcp_reader], views=[operation_latencies_view])
+        metrics.set_meter_provider(meter_provider)
 
-    def on_operation_complete(self, op: _CompletedOperationMetric) -> None:
-        super().on_operation_complete(op)
+    # def on_operation_complete(self, op: _CompletedOperationMetric) -> None:
+    #     super().on_operation_complete(op)
 
-        monitored_resource = MonitoredResource(type="bigtable_client_raw", labels={"zone": op.zone, **self.monitored_resource_labels})
-        if op.cluster_id is not None:
-            monitored_resource.labels["cluster"] = op.cluster_id
+    #     monitored_resource = MonitoredResource(type="bigtable_client_raw", labels={"zone": op.zone, **self.monitored_resource_labels})
+    #     if op.cluster_id is not None:
+    #         monitored_resource.labels["cluster"] = op.cluster_id
 
-    def on_attempt_complete(self, attempt: _CompletedAttemptMetric, op: _ActiveOperationMetric) -> None:
-        super().on_attempt_complete(attempt, op)
+    # def on_attempt_complete(self, attempt: _CompletedAttemptMetric, op: _ActiveOperationMetric) -> None:
+    #     super().on_attempt_complete(attempt, op)
 
-        monitored_resource = MonitoredResource(type="bigtable_client_raw", labels={"zone": op.zone, **self.monitored_resource_labels})
-        if op.cluster_id is not None:
-            monitored_resource.labels["cluster"] = op.cluster_id
+    #     monitored_resource = MonitoredResource(type="bigtable_client_raw", labels={"zone": op.zone, **self.monitored_resource_labels})
+    #     if op.cluster_id is not None:
+    #         monitored_resource.labels["cluster"] = op.cluster_id
