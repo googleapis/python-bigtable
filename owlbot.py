d489a0f1c3b0fad1c7003061d16c89449a76c7da
Daniel Sanche <d.sanche14@gmail.com>
2024-03-18 16:59:58 -0700

    fixed bad format

--- owlbot.py
+++ owlbot.py
@@ -254,7 +254,7 @@ insert(
     "google/cloud/bigtable_v2/services/bigtable/transports/__init__.py",
     '_transport_registry["grpc_asyncio"] = BigtableGrpcAsyncIOTransport',
     '_transport_registry["pooled_grpc_asyncio"] = PooledBigtableGrpcAsyncIOTransport',
-    '_transport_registry["rest"] = BigtableRestTransport'
+    '_transport_registry["rest"] = BigtableRestTransport',
     escape='[]"'
 )
 insert(
