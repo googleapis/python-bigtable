e39b97577c9f05ee0b55129c02828ceae0b6afa2
Daniel Sanche <d.sanche14@gmail.com>
2024-10-18 14:01:59 -0700

    updated runner

--- runner.py
+++ runner.py
@@ -2,9 +2,13 @@ from google.cloud.bigtable_v2 import BigtableAsyncClient
 import asyncio
 
 
+async def custom_callback(grpc_call):
+    print(await grpc_call.trailing_metadata())
+
+
 async def async_main():
     client = BigtableAsyncClient()
-    response1 = await client.ping_and_warm(name="projects/sanche-testing-project/instances/sanche-instance")
+    response1 = await client.ping_and_warm(name="projects/sanche-testing-project/instances/sanche-instance", raw_grpc_callback=custom_callback)
     print(response1)
 
 if __name__ == "__main__":
