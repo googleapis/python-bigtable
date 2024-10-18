48a22cde5c41458179e466435216bd1bb6a9d4ae
Daniel Sanche <d.sanche14@gmail.com>
2024-10-18 13:54:34 -0700

    updated runner

--- runner.py
+++ runner.py
@@ -5,7 +5,10 @@ import asyncio
 async def async_main():
     client = BigtableAsyncClient()
     response1 = await client.ping_and_warm(name="projects/sanche-testing-project/instances/sanche-instance")
+    response2 = client.ping_and_warm_w_call(name="projects/sanche-testing-project/instances/sanche-instance")
     print(response1)
+    print(await response2)
+    print(await response2.trailing_metadata())
 
 if __name__ == "__main__":
     asyncio.run(async_main())
