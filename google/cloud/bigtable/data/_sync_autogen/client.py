5a370dbac0da72d2f210074c896ba392b33ca5e6
Daniel Sanche <d.sanche14@gmail.com>
2025-03-07 15:40:49 -0800

    use different wrap method for sync

--- google/cloud/bigtable/data/_sync_autogen/client.py
+++ google/cloud/bigtable/data/_sync_autogen/client.py
@@ -66,6 +66,7 @@ from google.cloud.bigtable.data.row_filters import RowFilterChain
 from google.cloud.bigtable.data._cross_sync import CrossSync
 from typing import Iterable
 from grpc import insecure_channel
+from grpc import intercept_channel
 from google.cloud.bigtable_v2.services.bigtable.transports import (
     BigtableGrpcTransport as TransportType,
 )
@@ -282,7 +283,7 @@ class BigtableDataClient(ClientWithProject):
             start_timestamp = time.monotonic()
             old_channel = self.transport.grpc_channel
             new_channel = self.transport.create_channel()
-            new_channel._unary_unary_interceptors.append(self.transport._interceptor)
+            new_channel = intercept_channel(new_channel, self.transport._interceptor)
             self._ping_and_warm_instances(channel=new_channel)
             self.transport._grpc_channel = new_channel
             self.transport._logged_channel = new_channel
