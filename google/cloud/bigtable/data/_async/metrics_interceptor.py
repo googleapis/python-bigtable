5ba2bbe9960d8af83e29cec103df480f183412be
Daniel Sanche <d.sanche14@gmail.com>
2025-09-29 23:03:34 -0700

    pulled in improvements to data model

--- google/cloud/bigtable/data/_async/metrics_interceptor.py
+++ google/cloud/bigtable/data/_async/metrics_interceptor.py
@@ -13,10 +13,14 @@
 # limitations under the License
 from __future__ import annotations
 
+from typing import Sequence
+
 import time
 from functools import wraps
+
 from google.cloud.bigtable.data._metrics.data_model import ActiveOperationMetric
 from google.cloud.bigtable.data._metrics.data_model import OperationState
+from google.cloud.bigtable.data._metrics.data_model import OperationType
 from google.cloud.bigtable.data._metrics.handlers._base import MetricsHandler
 
 from google.cloud.bigtable.data._cross_sync import CrossSync
@@ -24,6 +28,7 @@ from google.cloud.bigtable.data._cross_sync import CrossSync
 if CrossSync.is_async:
     from grpc.aio import UnaryUnaryClientInterceptor
     from grpc.aio import UnaryStreamClientInterceptor
+    from grpc.aio import AioRpcError
 else:
     from grpc import UnaryUnaryClientInterceptor
     from grpc import UnaryStreamClientInterceptor
@@ -41,6 +46,7 @@ def _with_operation_from_metadata(func):
     @wraps(func)
     def wrapper(self, continuation, client_call_details, request):
         operation: "ActiveOperationMetric" | None = ActiveOperationMetric.get_active()
+
         if operation:
             # start a new attempt if not started
             if (
@@ -57,22 +63,26 @@ def _with_operation_from_metadata(func):
     return wrapper
 
 
-def _end_attempt(operation, exc, metadata):
-    """Helper to add metadata and exception to an operation"""
-    if metadata is not None:
-        operation.add_response_metadata(metadata)
-    if exc is not None:
-        # end attempt. If it succeeded, let higher levels decide when to end operation
-        operation.end_attempt_with_status(exc)
-
-
 @CrossSync.convert
-async def _get_metadata(source):
+async def _get_metadata(source) -> dict[str, str | bytes] | None:
     """Helper to extract metadata from a call or RpcError"""
     try:
-        return (await source.trailing_metadata() or []) + (
-            await source.initial_metadata() or []
-        )
+        metadata: Sequence[tuple[str, str | bytes]]
+        if CrossSync.is_async:
+            # grpc.aio returns metadata in Metadata objects
+            if isinstance(source, AioRpcError):
+                metadata = list(source.trailing_metadata()) + list(
+                    source.initial_metadata()
+                )
+            else:
+                metadata = list(await source.trailing_metadata()) + list(
+                    await source.initial_metadata()
+                )
+        else:
+            # sync grpc returns metadata as a sequence of tuples
+            metadata = source.trailing_metadata() + source.initial_metadata()
+        # convert metadata to dict format
+        return {k: v for (k, v) in metadata}
     except Exception:
         # ignore errors while fetching metadata
         return None
@@ -91,7 +101,12 @@ class AsyncBigtableMetricsInterceptor(
     async def intercept_unary_unary(
         self, operation, continuation, client_call_details, request
     ):
-        encountered_exc: Exception | None = None
+        """
+        Interceptor for unary rpcs:
+          - MutateRow
+          - CheckAndMutateRow
+          - ReadModifyWriteRow
+        """
         metadata = None
         try:
             call = await continuation(client_call_details, request)
@@ -99,39 +114,59 @@ class AsyncBigtableMetricsInterceptor(
             return call
         except Exception as rpc_error:
             metadata = await _get_metadata(rpc_error)
-            encountered_exc = rpc_error
             raise rpc_error
         finally:
-            _end_attempt(operation, encountered_exc, metadata)
+            if metadata is not None:
+                operation.add_response_metadata(metadata)
 
     @CrossSync.convert
     @_with_operation_from_metadata
     async def intercept_unary_stream(
         self, operation, continuation, client_call_details, request
     ):
-        async def response_wrapper(call):
-            has_first_response = operation.first_response_latency is not None
-            encountered_exc = None
-            try:
-                async for response in call:
-                    # record time to first response. Currently only used for READ_ROWs
-                    if not has_first_response:
-                        operation.first_response_latency_ns = (
-                            time.monotonic_ns() - operation.start_time_ns
-                        )
-                        has_first_response = True
-                    yield response
-            except Exception as e:
-                # handle errors while processing stream
-                encountered_exc = e
-                raise
-            finally:
-                if call is not None:
-                    _end_attempt(operation, encountered_exc, await _get_metadata(call))
-
+        """
+        Interceptor for streaming rpcs:
+          - ReadRows
+          - MutateRows
+          - SampleRowKeys
+        """
         try:
-            return response_wrapper(await continuation(client_call_details, request))
+            return self._streaming_generator_wrapper(
+                operation, await continuation(client_call_details, request)
+            )
         except Exception as rpc_error:
-            # handle errors while intializing stream
-            _end_attempt(operation, rpc_error, await _get_metadata(rpc_error))
+            metadata = await _get_metadata(rpc_error)
+            if metadata is not None:
+                operation.add_response_metadata(metadata)
             raise rpc_error
+
+    @staticmethod
+    @CrossSync.convert
+    async def _streaming_generator_wrapper(operation, call):
+        """
+        Wrapped generator to be returned by intercept_unary_stream
+        """
+        # only track has_first response for READ_ROWS
+        has_first_response = (
+            operation.first_response_latency_ns is not None
+            or operation.op_type != OperationType.READ_ROWS
+        )
+        encountered_exc = None
+        try:
+            async for response in call:
+                # record time to first response. Currently only used for READ_ROWs
+                if not has_first_response:
+                    operation.first_response_latency_ns = (
+                        time.monotonic_ns() - operation.start_time_ns
+                    )
+                    has_first_response = True
+                yield response
+        except Exception as e:
+            # handle errors while processing stream
+            encountered_exc = e
+            raise
+        finally:
+            if call is not None:
+                metadata = await _get_metadata(encountered_exc or call)
+                if metadata is not None:
+                    operation.add_response_metadata(metadata)
